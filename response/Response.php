a40dd919d90ee38eb76a7ae3ba6737e3b43cb6da
hikikomoriphoenix <dondon1987@yandex.com>
2018-08-11 20:49:35 +0800

    Wrap functions in class. Make functions static.

--- response/Response.php
+++ response/Response.php
@@ -1,34 +1,38 @@
 <?php
-header('Content-type: application/json');
-
-/**
- * Sets response code to HTTP 200 OK and outputs the response body in JSON 
- * format before finally ending the script execution. There should be no output
- * before this function is invoked, otherwise, all the previous output will be
- * erased.
- * 
- * @param Array $responseData An array that should contain the entire response
- * body.
- */
-function send($responseData) {
-    ob_clean();
-    http_response_code(200);
-    /* @var $response string */
-    $response = json_encode($responseData);
-    exit($response);
+if (!headers_sent()) {
+    header('Content-type: application/json');
 }
 
-/**
- * Sets response code to the given value and outputs given error message in a 
- * JSON formatted response body. Any previous output will be erased.
- *  
- * @param Integer $responseCode the appropriate response code for the error
- * @param String $message a string describing the error
- */
-function errorResponse($responseCode, $message) {
-    ob_clean();
-    http_response_code($responseCode);
-    /* @var $response string */
-    $response = json_encode(['message' => $message]);
-    exit($response);
+class Response {
+    /**
+     * Sets response code to HTTP 200 OK and outputs the response body in JSON 
+     * format before finally ending the script execution. There should be no output
+     * before this function is invoked, otherwise, all the previous output will be
+     * erased.
+     * 
+     * @param Array $responseData An array that should contain the entire response
+     * body.
+     */
+    static function send($responseData) {
+        ob_clean();
+        http_response_code(200);
+        /* @var $response string */
+        $response = json_encode($responseData);
+        exit($response);
+    }
+
+    /**
+     * Sets response code to the given value and outputs given error message in a 
+     * JSON formatted response body. Any previous output will be erased.
+     *  
+     * @param Integer $responseCode the appropriate response code for the error
+     * @param String $message a string describing the error
+     */
+    static function errorResponse($responseCode, $message) {
+        ob_clean();
+        http_response_code($responseCode);
+        /* @var $response string */
+        $response = json_encode(['message' => $message]);
+        exit($response);
+    }
 }
--- secure/Validate.php
+++ secure/Validate.php
@@ -1,32 +1,34 @@
 <?php
-/**
- * Checks if given username is alphanumeric and is at most 16 characters.
- * 
- * @param String $username the username to validate
- * @return Boolean true if username is valid.
- */
-function validateUsername($username) {
-    $result = preg_match("^[\w]{1,16}$", $username);
-    if ($result === 1) {
-        return true;
-    } else if ($result === 0 || $result === false) {
-        return false;
+class Validate {
+    /**
+     * Checks if given username is alphanumeric and is at most 16 characters.
+     * 
+     * @param String $username the username to validate
+     * @return Boolean true if username is valid.
+     */
+    static function validateUsername($username) {
+        $result = preg_match("^[\w]{1,16}$", $username);
+        if ($result === 1) {
+            return true;
+        } else if ($result === 0 || $result === false) {
+            return false;
+        }
     }
-}
 
-/**
- * Checks if given password does not contain any white space characters and
- * should be at least 6 and at most 16 characters long.
- * 
- * @param String $password the password to validate
- * @return Boolean true if password is valid.
- */
-function validatePassword($password) {
-    $result =  preg_match("^[\S]{6,16}$", $password); 
-    if ($result === 1) {
-        return true;
-    } else if ($result === 0 || $result === false) {
-        return false;
+    /**
+     * Checks if given password does not contain any white space characters and
+     * should be at least 6 and at most 16 characters long.
+     * 
+     * @param String $password the password to validate
+     * @return Boolean true if password is valid.
+     */
+    static function validatePassword($password) {
+        $result =  preg_match("^[\S]{6,16}$", $password); 
+        if ($result === 1) {
+            return true;
+        } else if ($result === 0 || $result === false) {
+            return false;
+        }
     }
 }
 
