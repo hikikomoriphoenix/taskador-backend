43bb5c24520fd8c1c008267e1b749a6d0a7a5161
hikikomoriphoenix <dondon1987@yandex.com>
2018-08-13 17:55:26 +0800

    Create assertions for response code and response body.

--- tests/response/ResponseTest.php
+++ tests/response/ResponseTest.php
@@ -2,16 +2,21 @@
 use PHPUnit\Framework\TestCase;
 
 class ResponseTest extends TestCase {
+    var $responseCode;
     public function testSend() {
         $url = 'http://localhost/taskador-backend/tests/response/200OKResponse.php';   
         $response = $this->sendRequest($url);
-        $this->handleResponse($response);
+        $expectedStatus = 'HTTP/1.1 200 OK';
+        $expectedBody = '{"number":4}';
+        $this->handleResponse($response, $expectedStatus, $expectedBody);
     }
     
     public function testErrorResponse() {
         $url = 'http://localhost/taskador-backend/tests/response/500ServerErrorResponse.php';
         $response = $this->sendRequest($url);
-        $this->handleResponse($response);
+        $expectedStatus = 'HTTP/1.1 500 Internal Server Error';
+        $expectedBody = '{"message":"Server Error!!!"}';
+        $this->handleResponse($response, $expectedStatus, $expectedBody);
     }
     
     private function sendRequest($url) {
@@ -23,13 +28,16 @@ class ResponseTest extends TestCase {
             'ignore_errors' => true
             ]];
         $context  = stream_context_create($options);
-        return file_get_contents($url, false, $context); 
+        $response = file_get_contents($url, false, $context);
+        $this->responseCode = $http_response_header[0];
+        return $response; 
     }
     
-    private function handleResponse($response) {
+    private function handleResponse($response, $expectedStatus, $expectedBody) {
         if ($response != false) {
-            echo $response;
             $this->assertJson($response);
+            $this->assertThat($this->responseCode, $this->equalTo($expectedStatus));
+            $this->assertThat($response, $this->equalTo($expectedBody));
         } else {
             $this->fail('Failed to get Response.');
         }
