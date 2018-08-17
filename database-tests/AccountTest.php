d160f033ea32c46487cb679926c4fa0a42585ec9
hikikomoriphoenix <dondon1987@yandex.com>
2018-08-17 11:19:51 +0800

    Test Account::addNewAccount.

--- database-tests/AccountTest.php
+++ database-tests/AccountTest.php
@@ -5,11 +5,30 @@ require_once 'ConnectLocal.php';
 use PHPUnit\Framework\TestCase;
 
 class AccountTest extends TestCase {
-    public function testUsernameIsUnique() {
-        $conn = ConnectLocal::connectToLocalhostDB();
-       
-        $username = 'test1'; //should be a unique username        
-        $unique = Account::usernameIsUnique($conn, $username);   
+    var $conn;
+    
+    protected function setUp() {
+        $this->conn = ConnectLocal::connectToLocalhostDB();        
+    }
+    
+    public function testAddNewAccount() {
+        $username = 'test3'; //should be a unique username
+        $password = Password::hashPassword('password');
+        $token = Token::generateToken(32);
+        $expiryDate = Token::getExpiryDate();
+        $lastActive = date("Y-m-d");
+        
+        try {
+            Account::addNewAccount($this->conn, $username, $password, $token, 
+                    $expiryDate, $lastActive); 
+            $this->assertTrue(true);
+        } catch (Exception $ex) {
+            $this->fail('Exception while adding account: ' . $ex->getMessage());
+        }       
+    }
+    public function testUsernameIsUnique() {       
+        $username = 'test4'; //should be a unique username        
+        $unique = Account::usernameIsUnique($this->conn, $username);   
         $this->assertTrue($unique, 'Given username already exists');
     }
 }
