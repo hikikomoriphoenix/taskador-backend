a6bebce4cf7e273c305b1859ec9f8bbe8d9ee62f
hikikomoriphoenix <dondon1987@yandex.com>
2018-08-23 01:39:14 +0800

    Test verifyPassword.

--- tests/secure/PasswordTest.php
+++ tests/secure/PasswordTest.php
@@ -8,5 +8,12 @@ class PasswordTest extends TestCase {
         $hash = Password::hashPassword('password');
         $this->assertThat(strlen($hash), $this->equalTo(60));
     }
+    
+    public function testVerifyPassword() {
+        $password = 'password';
+        $hash = '$2y$10$HZxaCFrGi9xVB9u7LhoevuPDtquuxjpiZbw/wogYHA4TjDG9LM83W';
+        $passIsCorrect = Password::verifyPassword($password, $hash);
+        $this->assertTrue($passIsCorrect);
+    }
 }
 
