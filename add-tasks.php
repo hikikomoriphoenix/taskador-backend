a0f4e6b3a4458c46077cdbd3fd798aff325c95fc
hikikomoriphoenix <dondon1987@yandex.com>
2018-08-30 01:23:15 +0800

    Create endpoint for adding tasks

---
+++ add-tasks.php
@@ -0,0 +1,12 @@
+<?php
+require_once 'autoload.php';
+
+if (filter_input(INPUT_SERVER, 'REQUEST_METHOD') === 'POST') {
+    // Get inputs
+    
+    // Connect to database
+    
+    // Check if token is authorized
+    
+    // Insert tasks to database    
+}
