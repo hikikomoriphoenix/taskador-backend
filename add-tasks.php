d0221dad38c0bbf0ad667cd15be18e342a0161b6
hikikomoriphoenix <dondon1987@yandex.com>
2018-08-30 02:27:05 +0800

    Get input from request for adding tasks

--- add-tasks.php
+++ add-tasks.php
@@ -3,7 +3,19 @@ require_once 'autoload.php';
 
 if (filter_input(INPUT_SERVER, 'REQUEST_METHOD') === 'POST') {
     // Get inputs
-    
+    $input = filter_input(INPUT_POST, 'input');
+    if (empty($input)) {
+        Response::errorResponse(400, 'Request has no input');
+    }    
+    /* @var $inputData array */
+    $inputData = json_decode($input);
+    /* @var $username string */
+    $username = $inputData['username'];
+    /* @var $token string */
+    $token = $inputData['token'];
+    /* @var $tasks array */
+    $tasks = $inputData['tasks'];
+   
     // Connect to database
     
     // Check if token is authorized
