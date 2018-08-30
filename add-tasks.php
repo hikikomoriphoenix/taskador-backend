6d684f10cb56597853103f7c8a6f9c589f2a1ab4
hikikomoriphoenix <dondon1987@yandex.com>
2018-08-30 14:38:58 +0800

    Verify token before adding tasks

--- add-tasks.php
+++ add-tasks.php
@@ -17,8 +17,29 @@ if (filter_input(INPUT_SERVER, 'REQUEST_METHOD') === 'POST') {
     $tasks = $inputData['tasks'];
    
     // Connect to database
+    try {
+        $conn = Connect::connectToTaskadorDB();
+    } catch (PDOException $e) {
+        Response::errorResponse(500, 'Exception on connecting to database: ' . 
+                $e->getMessage());
+    }    
     
     // Check if token is authorized
+    try {
+        $authorized = Verify::verifyToken($conn, $username, $token);
+    } catch (GetExpiryDateAndTokenFailureException $e) {
+        Response::errorResponse(500, $e->getMessage());
+    } catch (NoAccountException $e) {
+        Response::errorResponse(422, $e->getMessage());
+    } catch (ExpiredTokenException $e) {
+        Response::errorResponse(422, $e->getMessage());
+    } catch (NoTokenException $e) {
+        Response::errorResponse(500, $e->getMessage());
+    }
+    
+    if (!$authorized) {
+        Response::errorResponse(422, 'unauthorized token');
+    }
     
     // Insert tasks to database    
 }
