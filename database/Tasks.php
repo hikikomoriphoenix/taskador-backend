9125c48b5265e1fe5a803791bf4161d14d6936f7
hikikomoriphoenix <dondon1987@yandex.com>
2018-09-02 19:03:07 +0800

    Get the id of task last parsed for listing of task words

--- database/Tasks.php
+++ database/Tasks.php
@@ -148,5 +148,28 @@ class Tasks {
             throw $ex;
         }        
     }
+    
+    /**
+     * Get the id of a finished task that was last parsed for listing of words
+     * used in task statements or phrases.
+     * 
+     * @param PDO $conn connection to database
+     * @param type $username account username
+     * @return int id of a finished task
+     * @throws Exception
+     */
+    static function getIdOfLastParsedTask(PDO $conn, $username) {
+        $select = 'SELECT id_of_last_parsed_task FROM Accounts WHERE username ='
+                . " '$username';";
+        try {
+            $query = $conn->prepare($select);
+            $query->execute();
+            $result = $query->fetchAll();
+            $id = $result[0]['id_of_last_parsed_task'];
+            return $id;
+        } catch (Exception $ex) {
+            throw $ex;
+        }
+    }
 }
 
